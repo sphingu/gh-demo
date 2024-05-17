5bbecf5c4b56089f6ecb70f0dc975eee27a7d990
sphingu <sumit.hingu@getweave.com>
2024-05-17 23:30:41 +0530

    add deployment

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learan React
+          Learn React
         </a>
       </header>
     </div>
